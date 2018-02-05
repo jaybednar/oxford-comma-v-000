f2a8d4813733f55264bb622779eafaa3c988ce85
J B <ironviralmedia@gmail.com>
2018-02-05 18:06:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,13 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    def oxford_comma(array)
-  if array.length == 1
-    return array[0]
-  elsif array.length == 2
-    array.join(" and ")
-  else
     return a_s = "#{array[0..array.length - 3].join(", ")}, #{array[array.length - 2..array.length - 1].join(", and ")}"
-    
   end
+
 end
