5664bf4961b6b1683a590aae730d2115ab521f8e
J B <ironviralmedia@gmail.com>
2018-02-05 17:49:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,15 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2
     array.join(" and ")
+  else 
+    a_s = ""
+    until array[array.length - 2]
+      a_s << array.join(", ")
+    end 
+    while array{array.length - 1}
+      a_s << array.join(", and ")
+    end
+    a_s
   end
 
 end
